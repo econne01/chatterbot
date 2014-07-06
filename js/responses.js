5c54891be1e9afc08c3b849c43232ad7fca9803d
eric <eric.connelly@essencedigital.com>
2014-07-06 00:35:51 -0400

    Adding a few more canned responses

--- js/responses.js
+++ js/responses.js
@@ -3,6 +3,7 @@ var Responses = {
         'Hi :)',
         'Oh hey',
         'Hello there!',
+        'Hey! How\'s it going?',
         'Nice to meet you!'
     ],
     pleasantries: [
@@ -16,11 +17,16 @@ var Responses = {
     starterQuestions: [
         'Where are you from?',
         'What do you do for a living?',
+        'How is your day going?',
+        'What have you been up to lately?',
+        'So... how did it go?',
         'So how did you find me here?'
     ],
     answers: [
         'Oh, gosh. I don\'t know.',
         'I wish I knew',
+        'Not even Einstein knows that',
+        'Around 11 o\'clock, sir',
         'Psh, you know I can\'t tell you that'
     ]
 }
