2193b02aba435ad35717e0f85ccaaa756be50c1a
eric <eric.connelly@essencedigital.com>
2014-08-27 18:19:51 -0400

    Working in node.js to make this like a package and more portable and include imports and all that jazz

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    http = require('http'),
+    url = require('url'),
+    neo4j = require('node-neo4j');
+
+// @todo - possibly make this configurable from command line
+var port = 8888;
+var app = express();
+
+http.createServer(app);
+
+
+// Static assets
+app.use(express.static(__dirname));
+
+app.get('', function(request, response) {
+    response.sendfile(__dirname + '/index.html');
+});
+
+app.listen(port);
